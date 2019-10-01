3da59106614d512de748e1f5c6cc4240107ffd76
OsalG <56021380+OsalG@users.noreply.github.com>
2019-10-01 14:08:04 +0200

    master_client

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
+import { MysterylunchComponent } from './mysterylunch/mysterylunch.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'mysterylunch', component: MysterylunchComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
